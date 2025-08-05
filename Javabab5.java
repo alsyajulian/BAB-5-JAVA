53fac981438db16ec602ca7f1b06cdaf66f733ca
alsyajulian <alsyajulianarizki@gmail.com>
2025-08-05 11:09:41 +0700

    Add files via upload

---
+++ Javabab5.java
@@ -0,0 +1,181 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javabab5;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author PC 24 - LAB R1
+ */
+public class Javabab5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    
+//    Integer sebelum = 20;
+//    Integer sesudah = 0;
+//    
+//    System.out.println("Sebelum Increment :"+ sebelum);
+//    sesudah = ++sebelum;
+//    System.out.println("Sesudah Increment :"+ sesudah);
+    
+    
+//    Integer sebelum = 9;
+//    Integer sesudah = 0;
+//    
+//    System.out.println("Sebelum Decrement : " + sebelum);
+//    sesudah = --sebelum;
+//    System.out.println("sesudah Decrement :" + sesudah);
+//    
+    
+    
+//      int a = 2;
+//      int b = 0;
+//      
+//      System.out.println("Sebelum negasi : "+ a);
+//      b = -a;
+//      System.out.println("Sebelum negasi :"+ b);
+// 
+
+
+//
+//       //Penambahan
+//       int a1 = 10;
+//       int a2 = 2;
+//       System.out.println("Penambahan 10 + 2 : " + (a1 + a2));
+//       //Pengurangan
+//        int b1 = 10;
+//        int b2 = 2;
+//       System.out.println("Pengurangan 10 - 2 : " + (b1 - b2));
+//       //Perkalian
+//       int c1 = 16;
+//       int c2 = 2;
+//       System.out.println("Perkalian 16 * 2 : " + (c1 * c2));
+//       //Pembagian
+//       int d1 = 16;
+//       int d2 = 2;
+//       System.out.println("Pembagian 16 / 2 : " + (d1 / d2));
+//       //Sisa Bagi
+//       int e1 = 16;
+//       int e2 = 2;
+//       System.out.println("Sisa bagi 16 dan 2 : " + (e1 %e2));
+
+
+
+//       int a = 10;
+//       int b = 15;
+//       
+//       System.out.println("Variabel a : " + a);
+//       System.out.println("Variabel b : " + b);
+//       System.out.println("Apakah 10 kurang dari 15 " + (a < b));
+//       System.out.println("Apakah 10 kurang dari 15 " + (b < a));
+//       System.out.println("Apakah 10 kurang dari 10 " + (b < b));
+       
+       
+       
+//       int a = 2;
+//       int b = 3;
+//       System.out.println("Variabel a : " + a);
+//       System.out.println("Variabel b : " + b);
+//       System.out.println("Apakah 2 lebih besar dari 3 " + (a > b));
+//       System.out.println("Apakah 3 lebih besar dari 2 " + (b < a));
+//       System.out.println("Apakah 3 lebih besar dari 3 " + (b < b));
+       
+       
+//       
+//       int a = 3;
+//       int b = 3;
+//       System.out.println("Variabel a : " + a);
+//       System.out.println("Variabel b : " + b);
+//       System.out.println("Apakah 3 lebih kecil atau sama dengan 3 " + (a <= b));
+//       a = 9;
+//       System.out.println("Variabel a diubah : " + a);
+//       System.out.println("Apakah 9 lebih kecil atau sama dengan 3 " + (a <= b));
+       
+       
+       
+//      int a = 3;
+//      int b = 3;
+//      System.out.println("Variabel a : " + a);
+//      System.out.println("Variabel b : " + b);
+//      System.out.println("Apakah 3 lebih besar atau sama dengan 3 " + (a >= b));
+//      a = 2;
+//      System.out.println("Variabel a diubah : " + a);
+//      System.out.println("Apakah 2 lebih besar atau sama dengan 3 " + (a >= b)); 
+       
+
+
+
+//       int a = 3;
+//       int b = 3;
+//       System.out.println("Variabel a : " + a);
+//       System.out.println("Variabel b : " + b);
+//       System.out.println("Apakah 3 sama dengan 3 " + (a == b)); a = 2;
+//       System.out.println("Apakah 2 sama dengan 3 " + (a == b));
+//       System.out.println("Apakah 2 tidak sama dengan 3 " + (a != b));
+
+
+
+//        System.out.println("true and true : " + (true && true));
+//        System.out.println("true and false : " + (true && false));
+//        System.out.println("false and false : " + (false && false));
+//        System.out.println("false and true : " + (false && true));
+//        
+//        
+//        
+//       System.out.println("true or true : " + (true || true));
+//       System.out.println("true or false : " + (true || false));
+//       System.out.println("false or false : " + (false || false));
+//       System.out.println("false or true : " + (false || true)); 
+
+
+
+//        String temp = JOptionPane.showInputDialog(null,
+//        "Masukan Angka", "Menentukan Ganjil Genap",
+//        JOptionPane.QUESTION_MESSAGE);
+//        int input = Integer.parseInt(temp);
+//        System.out.println((input %2)); if ((input % 2) == 0) {
+//        JOptionPane.showMessageDialog(null, "Angka : " + input + " "+ "adalah bilangan GENAP", "Informasi",
+//        JOptionPane.INFORMATION_MESSAGE);} else if ((input % 2) == 1) 
+//        JOptionPane.showMessageDialog(null, "Angka : "+ input + " "+ "adalah bilangan GANJIL", "Informasi",JOptionPane.INFORMATION_MESSAGE);
+
+
+
+
+//          String umur = JOptionPane.showInputDialog(null, "Kamu umur berapa ?");
+//          if (Integer.parseInt(umur) <= 5) {JOptionPane.showMessageDialog(null, "Balita");} else if (Integer.parseInt(umur) >= 6 && Integer.parseInt(umur) <= 11){ JOptionPane.showMessageDialog(null, "Anak - anak");
+//          } else if (Integer.parseInt(umur) >= 12 && Integer.parseInt(umur) <= 25){ JOptionPane.showMessageDialog(null, "Remaja");
+//          } else {JOptionPane.showMessageDialog(null, "Dewasa");}
+     
+            
+
+    
+//           String nilai = JOptionPane.showInputDialog
+//            (null, "Masukan nilai :");
+//            switch (nilai.toUpperCase()) {
+//            case "A":
+//            JOptionPane.showMessageDialog(null, "Sangat baik.");
+//            break;
+//            case "B":
+//            JOptionPane.showMessageDialog(null, "Baik.");
+//            break;
+//            case "C":
+//            JOptionPane.showMessageDialog(null, "Cukup.");
+//            case "D":
+//            JOptionPane.showMessageDialog(null, "Jelek.");
+//            break;
+//            default :
+//            JOptionPane.showMessageDialog(null,
+//            "Salah memasukan nilai !"); 
+//}
+     
+    }
+    
+}
